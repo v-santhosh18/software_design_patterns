df05fc35699100a0f02f1ff277c154520a454b47
Santhosh Velmurugan <151381929+v-santhosh18@users.noreply.github.com>
2024-08-06 10:58:02 +0530

    backend update

--- sdp/Backend/src/main/java/com/security/template/auth/AuthenticationController.java
+++ sdp/Backend/src/main/java/com/security/template/auth/AuthenticationController.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/v1/auth/")
